623111a922c3086f98996236034ae79d7a05e625
bernardo <bernardodemsj@gmail.com>
2018-07-18 16:44:22 -0300

    1 e 2 pedidos com calculo pronto e mensagem intermetdiária

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,7 @@ process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
 	const agent = new WebhookClient({ request, response });
+	// console.log(agent);
 	const queryResult = request.body.queryResult;
 	const parameter = request.body.queryResult.parameters;
 	const actions = request.body.queryResult.action;
@@ -41,89 +42,48 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   }
 
 	function comprar_dois_pedidos(agent){
-		console.log("entrou na funcao correta");
-	    const contexto_nome = get_slot_atual(agent);
-	    let resposta = "";
+		const contador_entidade = agent.getContext("primeiro_acesso_intencao");
+		console.log("intecao "+ contador_entidade);
+	  const contexto_nome = get_slot_atual(agent);
+		console.log("nomes do contexto " + contexto_nome);
+	  let resposta = "";
 		if (contexto_nome === "qtd1" || contexto_nome === "qtd2") {
-			console.log("ENTROU NO LUGAR CORRETO");
 			agent.outgoingContexts_ = [];
 			agent.contexts = []
-		}else{
-			const itens = get_lista_pedidos(agent,2);
-			console.log("mensagem pedidos: ",itens);
-		    switch (contexto_nome) {
-				case "sabor1":
-					resposta = resolve_slot(agent, "sabor1", "sabor");
-					break;
-				case "tamanho1":
-					resposta = resolve_slot(agent, "tamanho1", "tamanho");
-					break;
-				case "sabor2":
-		        	resposta = resolve_slot(agent, "sabor2", "sabor");
-					break;
-				case "tamanho2":
-					resposta = resolve_slot(agent, "tamanho2", "tamanho");
-		      		break;
-		    }
-			//agent.add(mensagem_pedidos);
+			agent.add("")
+			return;
+		}
+		const itens = get_lista_pedidos(agent,2);
+		console.log("mensagem pedidos: ",itens);
+		if (contador_entidade === null) {
+			agent.setContext({ name: "primeiro_acesso_intencao", lifespan: 2});
+			const message = get_message(itens)
+			agent.add(message)
+		}
+    switch (contexto_nome) {
+			case "sabor1":
+				resposta = resolve_slot(agent, "sabor1", "sabor");
+				break;
+			case "tamanho1":
+				resposta = resolve_slot(agent, "tamanho1", "tamanho");
+				break;
+			case "sabor2":
+	      resposta = resolve_slot(agent, "sabor2", "sabor");
+				break;
+			case "tamanho2":
+				resposta = resolve_slot(agent, "tamanho2", "tamanho");
+      	break;
+    }
+		const required_params = queryResult.allRequiredParamsPresent;
+		if (required_params === true) {
 			const total = calcular_conta(itens);
 		}
+
 		if (resposta !== ""){
 			agent.add(resposta);
 		}
 	}
 
-	// /**
-	// * metodo no limbo
-	// */
-	// function set_mensagem_pedidos(agent, pedidos){
-	// 	const parametros = agent.contexts[0].parameters;
-	// 	let lista_mensagens = [];
-	// 	let itens = [];//[]
-	// 	for (let i = 1; i <= pedidos; i++) {
-	// 		pedido = {};
-	// 		let j = i.toString()
-	// 		const qtd = parametros["qtd" + j]["number"];
-	// 		if (qtd === 1){
-	// 			const msg_qtd = "uma pizza ";
-	// 			pedido["qtd"] = qtd;
-	//
-	// 		}else if (qtd === "outras") {
-	// 			const qtd_total = parametros["qtdTotal"]
-	// 			pedido["qtd_total"] = qtd_total
-	// 		}else {
-	// 			const msg_qtd = qtd.toString() + " pizzas ";
-	// 			pedido["qtd"] = qtd;
-	// 		}
-	// 		let mensagem_pedido = "o " + j + "º pedido " +  ": " + msg_qtd;
-	// 		const sabor = parametros["sabor" + j];
-	// 		const tamanho = parametros["tamanho" + j];
-	// 		if (tamanho !== "") {
-	// 			mensagem_pedido.concat(tamanho + " ");
-	// 			pedido["tamanho"] = tamanho;
-	// 		}
-	// 		if (sabor !== "") {
-	// 			mensagem_pedido.concat("de " + sabor + " ");
-	// 			pedido["sabor"] = sabor;
-	// 		}
-	// 		const massa = parametros["massa" + j];
-	// 		if (massa !== "") {
-	// 			mensagem_pedido.concat("com massa " + massa + " ");
-	// 			pedido["massa"] = massa;
-	// 		}
-	// 		const borda = parametros["borda" + j];
-	// 		if (borda !== "") {
-	// 			mensagem_pedido.concat("e borda de " + borda);
-	// 			pedido["borda"] = borda;
-	// 		}
-	// 		lista_mensagens.push(mensagem_pedido);
-	// 		itens.push(pedido);
-	// 	}
-	// 	console.log(lista_mensagens);
-	// 	const mensagem_final = "Indentifiquei 2 pedidos: " + lista_mensagens[0] + " e o " + lista_mensagens[1]
-	// 	console.log(mensagem_final);
-	// 	return (mensagem_final);
-	// }
 
 	function get_lista_pedidos(agent, qtd_pedidos){
 		const parametros = agent.contexts[0].parameters;
@@ -171,13 +131,13 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 			}else {
 				const quantidade = item.qtd;
 				pizzas_pedidas = pizzas_pedidas + quantidade;
-			mensagem = mensagem.concat(` o ${index.toString}º pedido: ${quantidade} pizzas`)
+			mensagem = mensagem.concat(` o ${(index+1).toString()}º pedido: ${quantidade} pizza(s)`)
 			}if (item.hasOwnProperty("tamanho")){
 				const tamanho = item.tamanho;
 				mensagem = mensagem.concat(` ${tamanho}`)
 			}if (item.hasOwnProperty("sabor")){
 				const sabor = item.sabor;
-				mensgem = mensagem.concat(` de ${sabor}`)
+				mensagem = mensagem.concat(` de ${sabor}`)
 			}if (item.hasOwnProperty("massa")){
 				const massa = item.massa;
 				mensagem = mensagem.concat(` com massa ${massa}`)
@@ -185,15 +145,16 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 				const borda = item.borda;
 				mensagem = mensagem.concat(` e borda ${borda}`)
 			}
-			mensagens.push(mensagem)
+			mensagens.push(mensagem + )
 		})
-		const mensagem_final = "Indentifiquei 2 pedidos: " + mensagens[0] + " e o " + mensagens[1]
+		const mensagem_final = "Indentifiquei 2 pedidos: " + mensagens[0] + " e o " + mensagens[1] + "."
+		return mensagem_final;
 	}
 
 	function get_slot_atual(agent){
 		try {
 			const contexto = agent.contexts.filter( (context) => context.name.includes('_dialog_params_'))[0];
-			console.log('contexto get slot atual ' + JSON.stringify(contexto));
+			// console.log('contexto get slot atual ' + JSON.stringify(contexto));
 			const contexto_nome = contexto.name.split('_').pop();
 			return contexto_nome;
 		} catch(e) {
@@ -207,28 +168,25 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
 	}
 
-	function resolve_slot(agent, entidade, msg, primeira_mensagem) {
-		// const primeira_mensagem = agent.getContext("primeira_mensagem")
+	function resolve_slot(agent, entidade, msg) {
 		const contexto = agent.getContext(entidade);
-		// console.log("contexto " + JSON.stringify(contexto, null, 4));
-		// console.log(agent.contexts[0].parameters);
 		const contador_entidade = entidade.concat("qtd");
 		let parametro = {};
-	    if (contexto === null){
+    if (contexto === null){
 			parametro[contador_entidade] = 1;
-			console.log(parametro);
+			// console.log(parametro);
 			agent.setContext({ name: entidade, lifespan: 2, parameters: parametro});
 			return "";
-	    }
-	    const quantidade  = contexto.parameters[contador_entidade];
-	    if (quantidade === 1){
+    }
+    const quantidade  = contexto.parameters[contador_entidade];
+    if (quantidade === 1){
 			parametro[contador_entidade] = quantidade + 1;
-			console.log(parametro);
-	    	agent.setContext({ name: entidade, lifespan: 2, parameters: parametro});
-	    	return "Não consegui entender, pode repetir por favor?";
-	    }else {
-	    	return `Não temos esse ${msg}`;
-	    }
+			// console.log(parametro);
+    	agent.setContext({ name: entidade, lifespan: 2, parameters: parametro});
+    	return "Não consegui entender, pode repetir por favor?";
+    }else {
+    	return `Não temos esse ${msg}`;
+    }
 	}
 
 	function calcular_conta (itens) {
@@ -236,25 +194,28 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 		let pizzas_total = 0
 
 		itens.forEach( function(pedido) {
-				var valorBorda = 0;
-				sabor = cardapio.pizzas[pedido.sabor];
-				valorPizza = sabor.tamanho[pedido.tamanho];
+				let valorBorda = 0;
+				// console.log(`pedido: ${JSON.stringify(pedido)}`);
+				let sabor = cardapio.pizzas[pedido["sabor"]];
+				let valorPizza = sabor.tamanho[pedido.tamanho];
 				try {
-					borda = cardapio.borda[pedido.borda];
+					let borda = cardapio.borda[pedido.borda];
 					valorBorda = borda.tamanho[pedido.tamanho];
 				} catch (e) {
 					valorBorda = 0;
 				}
 
 				if(pedido.hasOwnProperty("qtd_total")){
-					element["qtd"] = element["qtd_total"] - pizzas_total;
+					pedido["qtd"] = pedido["qtd_total"] - pizzas_total;
 				}else{
-					pizzas_total += element["qtd"];
-					valorTotal = valorTotal + element["qtd"]*(valorPizza + valorBorda);
+					pizzas_total += pedido["qtd"];
+					valorTotal = valorTotal + pedido["qtd"]*(valorPizza + valorBorda);
 				}
+				console.log("qtd: "+ pedido["qtd"]);
+				console.log("borda "+valorBorda);
+				console.log("Pizza "+valorPizza);
 
 		});
-
 		console.log("Valor Total .. " + valorTotal);
 
 	}
